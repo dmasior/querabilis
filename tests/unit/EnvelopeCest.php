e9000a75c9d47a0062cb540a3784a030559b0547
initx <solver89@gmail.com>
2019-05-29 22:49:30 +0200

    envelope unit test

---
+++ tests/unit/EnvelopeCest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use DateTime;
+use Initx\Envelope;
+use Tests\UnitTester;
+
+class EnvelopeCest
+{
+    public function newInstance(UnitTester $I)
+    {
+        // arrange
+        $payload = 'some text';
+        $title = 'any title';
+        $timestamp = new DateTime();
+
+        // act
+        $envelope = new Envelope($payload, $title, $timestamp);
+
+        // assert
+        $I->assertSame($payload, $envelope->getPayload());
+        $I->assertSame($title, $envelope->getTitle());
+        $I->assertSame($timestamp, $envelope->getTimestamp());
+    }
+}
